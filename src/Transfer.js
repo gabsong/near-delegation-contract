734f91259953af3d6a2eaaa2c268fbd86472ad2b
Gabriel Song <gabsong4@gmail.com>
2020-04-20 00:08:37 -0700

    Initial commit

---
+++ src/Transfer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Transfer = (props) => {
+  
+  return (
+    <React.Fragment>
+      <div>Balance</div>
+      <div>Deposit</div>
+      <div>Withdraw</div>
+    </React.Fragment>
+  );  
+};
+
+export default Transfer;
