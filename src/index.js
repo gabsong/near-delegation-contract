d597290a128efb0b89e963eb5a3f90839c18dfa9
Gabriel Song <gabsong4@gmail.com>
2020-04-29 13:29:39 -0700

    Update file paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App2';
+import App from './App';
 import getConfig from './config.js';
 import * as nearlib from 'near-api-js';
 
